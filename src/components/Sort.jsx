ec657888fcadd3e9e7016ef2cd94df1ea736d74f
Anton <toxajlb@yandex.ru>
2023-03-14 17:08:19 +0300

    created a cart component

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSort } from '../redux/slices/filterSlice';
 
@@ -16,14 +16,28 @@ const Sort = () => {
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filter.sort);
   const [isVisible, setIsVisible] = useState(false);
+  const sortRef = useRef();
 
   const onClickItem = (obj) => {
     dispatch(setSort(obj));
     setIsVisible(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.composedPath().includes(sortRef.current)) {
+        setIsVisible(false);
+      }
+    };
+    document.body.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  totalPrice: 0,
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1,
+        });
+      }
+
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
+    },
+    minusItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload);
+
+      if (findItem) {
+        findItem.count--;
+        state.totalPrice -= findItem.price;
+      }
+    },
+    removeItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload);
+
+      state.items = state.items.filter((obj) => obj.id !== action.payload);
+      state.totalPrice -= findItem.price * findItem.count;
+    },
+    clearItems(state) {
+      state.items = [];
+      state.totalPrice = 0;
+    },
+  },
+});
+
+export const { addItem, minusItem, removeItem, clearItems } = cartSlice.actions;
+
+export default cartSlice.reducer;
